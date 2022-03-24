0bcc7c8abae9de156a3d57b574669b97922b4c4b
Christie Lewis <christielewis@Christies-Air.attlocal.net>
2022-03-24 12:27:03 -0400

    Created auth folder; created api.js, events.js, and ui.js files and added t auth folder

---
+++ app/auth/api.js
@@ -0,0 +1,49 @@
+// const store = require('../store.js')
+
+const signUp = function (data) {
+    console.log(store)
+    return $.ajax({
+        method: 'POST',
+        url: 'https://tic-tac-toe-api-development.herokuapp.com/sign-up',
+        data
+        // same as data: data
+    })
+}
+
+const signIn = function (data) {
+    return $.ajax({
+        method: 'POST',
+        url: 'https://tic-tac-toe-api-development.herokuapp.com/sign-in',
+        data
+        // same as data: data
+    })
+}
+
+const changePassword = function (data) {
+    return $.ajax({
+        method: 'PATCH',
+        url: 'https://tic-tac-toe-api-development.herokuapp.com/change-password',
+        headers: {
+            Authorization: 'Bearer ' + store.user.token
+        },
+        data
+        // same as data: data
+    })
+}
+
+const signOut = function () {
+    return $.ajax({
+        method: 'DELETE',
+        url: 'https://tic-tac-toe-api-development.herokuapp.com/sign-out',
+        headers: {
+            Authorization: 'Bearer ' + store.user.token
+        }
+    })
+}
+
+module.exports = {
+    signUp,
+    signIn,
+    changePassword,
+    signOut
+}
