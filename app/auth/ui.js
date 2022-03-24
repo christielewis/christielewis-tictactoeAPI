aef1b60e13f61972c89f476da537d8ae48a900b7
Christie Lewis <christielewis@Christies-Air.attlocal.net>
2022-03-24 14:34:43 -0400

    greated separate sections for sign-in/up/out change-pw display

--- app/auth/ui.js
+++ app/auth/ui.js
@@ -1,12 +1,12 @@
 const store = require('../store.js')
 
 const onSignUpSuccess = function() {
-    $('#auth-status').html('<p>Sign Up Successful!</p>')
+    $('#auth-status-sign-up').html('<p>Sign Up Successful!</p>')
     $('form').trigger('reset')
 }
 
 const onSignUpFailure = function() {
-    $('#auth-status').html('<p>There was an error signing up.<br>Try again!</p>')
+    $('#auth-status-sign-up').html('<p>There was an error signing up.<br>Try again!</p>')
 }
 
 module.exports = {
