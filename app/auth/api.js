f64057f5c3eff79c484c6669d65557bfddcd3faa
Christie Lewis <christielewis@Christies-Air.attlocal.net>
2022-03-24 12:35:08 -0400

    removed ajax from api.js

--- app/auth/api.js
+++ app/auth/api.js
@@ -9,38 +9,6 @@ const signUp = function (data) {
     })
 }
 
-const signIn = function (data) {
-    return $.ajax({
-        method: 'POST',
-        url: 'https://tic-tac-toe-api-development.herokuapp.com/sign-in',
-        data
-    })
-}
-
-const changePassword = function (data) {
-    return $.ajax({
-        method: 'PATCH',
-        url: 'https://tic-tac-toe-api-development.herokuapp.com/change-password',
-        headers: {
-            Authorization: 'Bearer ' + store.user.token
-        },
-        data
-    })
-}
-
-const signOut = function () {
-    return $.ajax({
-        method: 'DELETE',
-        url: 'https://tic-tac-toe-api-development.herokuapp.com/sign-out',
-        headers: {
-            Authorization: 'Bearer ' + store.user.token
-        }
-    })
-}
-
 module.exports = {
     signUp,
-    signIn,
-    changePassword,
-    signOut
 }
